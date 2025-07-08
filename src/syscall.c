6bdd8fe47c4539a625ae9b1a9d0fade77d0ef34e
ayuly0 <85594469+ayuly0@users.noreply.github.com>
2025-07-08 15:50:35 +0700

    refactor & implemnt: syscall_read

--- src/syscall.c
+++ src/syscall.c
@@ -3,20 +3,26 @@
 
 Err invoke_syscall( AYM *vm )
 {
-    VmSyscall syscall_code = vm->stack[ --vm->registers[ REG_ESP ].as_u64 ].as_u64;
+    VmSyscall syscall_code = vm->registers[ REG_0 ].as_u64;
     switch ( syscall_code )
     {
     case SYSCALL_EXIT: {
-        u64 code = vm->stack[ --vm->registers[ REG_ESP ].as_u64 ].as_u64;
+        u64 code = vm->registers[ REG_1 ].as_u64;
         vm->halt = true;
         break;
     }
     case SYSCALL_WRITE: {
-        u64 len     = vm->stack[ --vm->registers[ REG_ESP ].as_u64 ].as_u64;
-        u64 buf_val = vm->stack[ --vm->registers[ REG_ESP ].as_u64 ].as_u64;
-        u64 fd      = vm->stack[ --vm->registers[ REG_ESP ].as_u64 ].as_u64;
-        void *buf   = ( void * )( uintptr_t )buf_val;
+        u64 len   = vm->registers[ REG_ESP ].as_u64;
+        void *buf = vm->registers[ REG_ESP ].as_ptr;
+        u64 fd    = vm->registers[ REG_ESP ].as_u64;
         write( fd, buf, len );
+        break;
+    }
+    case SYSCALL_READ: {
+        u64 len   = vm->registers[ REG_3 ].as_u64;
+        void *buf = vm->registers[ REG_2 ].as_ptr;
+        u64 fd    = vm->registers[ REG_1 ].as_u64;
+        read( fd, buf, len );
         break;
     }
     default: fprintf( stderr, "Unknown syscall %d\n", syscall_code ); return ERR_UNKOWN_SYSCALL;
